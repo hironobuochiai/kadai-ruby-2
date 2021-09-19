e9fdb315c24bb8ce92b4b7e31b86eb4b95356a7b
hironobu.ochiai <hironobuochiai113@gmail.com>
2021-09-19 06:15:13 +0000

    kadai-fizzbuzz rev

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,16 @@
 def fizzbuzz(num)
     if num%(3*5) == 0
-        puts 'FizzBuzz'
+        return 'FizzBuzz'
     elsif num%3 == 0
-        puts 'Fizz'
+        return 'Fizz'
     elsif num%5 == 0
-        puts 'Buzz'
+        return 'Buzz'
     else 
-        puts num
+        return num
     end
 end
 
-num_max = (100)
-(1..num_max).each do |num|
-    fizzbuzz(num)
+num_max = 100
+(1..num_max).each do |number|
+   puts fizzbuzz(number)
 end
-    
